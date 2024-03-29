ec2a19ce2e0ba8894778ed79541d4e4a47bd92e2
Diego Grajeda <128556830+Byskull2000@users.noreply.github.com>
2024-03-28 23:46:15 -0400

    NoraaDBV1

---
+++ noraaDB1.0.sql
@@ -0,0 +1,331 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : postgre
+ Source Server Type    : PostgreSQL
+ Source Server Version : 120017 (120017)
+ Source Host           : localhost:5432
+ Source Catalog        : noraadb
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 120017 (120017)
+ File Encoding         : 65001
+
+ Date: 28/03/2024 22:37:37
+*/
+
+
+-- ----------------------------
+-- Sequence structure for Menu_id_menu_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."Menu_id_menu_seq";
+CREATE SEQUENCE "public"."Menu_id_menu_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for platillo_id_platillo_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."platillo_id_platillo_seq";
+CREATE SEQUENCE "public"."platillo_id_platillo_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for registro_avistamiento_id_avistamiento_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."registro_avistamiento_id_avistamiento_seq";
+CREATE SEQUENCE "public"."registro_avistamiento_id_avistamiento_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for resena_restaurante_id_resena_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."resena_restaurante_id_resena_seq";
+CREATE SEQUENCE "public"."resena_restaurante_id_resena_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for restaurante_id_restaurante_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."restaurante_id_restaurante_seq";
+CREATE SEQUENCE "public"."restaurante_id_restaurante_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for usuario_id_usuario_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."usuario_id_usuario_seq";
+CREATE SEQUENCE "public"."usuario_id_usuario_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for consumible
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."consumible";
+CREATE TABLE "public"."consumible" (
+  "id_consumible" int4 NOT NULL DEFAULT nextval('platillo_id_platillo_seq'::regclass),
+  "nombre_consumible" varchar(150) COLLATE "pg_catalog"."default" NOT NULL,
+  "precio_consumible" numeric(4,2) NOT NULL,
+  "foto_consumible" text COLLATE "pg_catalog"."default" NOT NULL,
+  "descripcion_consumible" varchar(255) COLLATE "pg_catalog"."default",
+  "id_menu_fk" int4 NOT NULL,
+  "valoracion_consumible" numeric(1,1) NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of consumible
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for etiqueta
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."etiqueta";
+CREATE TABLE "public"."etiqueta" (
+  "id_etiqueta" int4 NOT NULL,
+  "etiqueta_nombre" varchar(30) COLLATE "pg_catalog"."default" NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of etiqueta
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for etiqueta_restaurante
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."etiqueta_restaurante";
+CREATE TABLE "public"."etiqueta_restaurante" (
+  "id_restaurante_etiquetado" int4 NOT NULL,
+  "id_etiqueta_restaurante" int4 NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of etiqueta_restaurante
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."menu";
+CREATE TABLE "public"."menu" (
+  "id_menu" int4 NOT NULL DEFAULT nextval('"Menu_id_menu_seq"'::regclass),
+  "nombre_menu" varchar(100) COLLATE "pg_catalog"."default" NOT NULL,
+  "vigente" bool NOT NULL,
+  "id_restaurante_fk" int4 NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for registro_avistamiento
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."registro_avistamiento";
+CREATE TABLE "public"."registro_avistamiento" (
+  "id_avistamiento" int4 NOT NULL DEFAULT nextval('registro_avistamiento_id_avistamiento_seq'::regclass),
+  "nombre_restaurante" varchar(100) COLLATE "pg_catalog"."default" NOT NULL,
+  "foto" text COLLATE "pg_catalog"."default",
+  "direccion" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "id_usuario_fk" int4 NOT NULL,
+  "coordenada_longitud" numeric NOT NULL,
+  "coordenada_latitud" numeric NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of registro_avistamiento
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for resena_restaurante
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."resena_restaurante";
+CREATE TABLE "public"."resena_restaurante" (
+  "id_resena" int4 NOT NULL DEFAULT nextval('resena_restaurante_id_resena_seq'::regclass),
+  "id_usuario" int4,
+  "id_restaurante" int4,
+  "calificacion" int4,
+  "comentario" text COLLATE "pg_catalog"."default",
+  "verificado" bool
+)
+;
+
+-- ----------------------------
+-- Records of resena_restaurante
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for restaurante
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."restaurante";
+CREATE TABLE "public"."restaurante" (
+  "id_restaurante" int4 NOT NULL DEFAULT nextval('restaurante_id_restaurante_seq'::regclass),
+  "nombre_restaurante" varchar(100) COLLATE "pg_catalog"."default" NOT NULL,
+  "direccion" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "telefono" varchar(15) COLLATE "pg_catalog"."default" NOT NULL,
+  "horario_atencion" varchar(100) COLLATE "pg_catalog"."default" NOT NULL,
+  "coordenada_longitud" numeric NOT NULL,
+  "coordenada_latitud" numeric NOT NULL,
+  "valoracion" numeric(1,1) NOT NULL,
+  "imagen" text COLLATE "pg_catalog"."default" NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of restaurante
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for usuario
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."usuario";
+CREATE TABLE "public"."usuario" (
+  "id_usuario" int4 NOT NULL DEFAULT nextval('usuario_id_usuario_seq'::regclass),
+  "nombre" varchar(50) COLLATE "pg_catalog"."default",
+  "apellido" varchar(50) COLLATE "pg_catalog"."default",
+  "telefono" varchar(15) COLLATE "pg_catalog"."default",
+  "correo_electronico" varchar(100) COLLATE "pg_catalog"."default",
+  "contrasena" varchar(100) COLLATE "pg_catalog"."default"
+)
+;
+
+-- ----------------------------
+-- Records of usuario
+-- ----------------------------
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."Menu_id_menu_seq"
+OWNED BY "public"."menu"."id_menu";
+SELECT setval('"public"."Menu_id_menu_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."platillo_id_platillo_seq"
+OWNED BY "public"."consumible"."id_consumible";
+SELECT setval('"public"."platillo_id_platillo_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."registro_avistamiento_id_avistamiento_seq"
+OWNED BY "public"."registro_avistamiento"."id_avistamiento";
+SELECT setval('"public"."registro_avistamiento_id_avistamiento_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."resena_restaurante_id_resena_seq"
+OWNED BY "public"."resena_restaurante"."id_resena";
+SELECT setval('"public"."resena_restaurante_id_resena_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."restaurante_id_restaurante_seq"
+OWNED BY "public"."restaurante"."id_restaurante";
+SELECT setval('"public"."restaurante_id_restaurante_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."usuario_id_usuario_seq"
+OWNED BY "public"."usuario"."id_usuario";
+SELECT setval('"public"."usuario_id_usuario_seq"', 1, false);
+
+-- ----------------------------
+-- Primary Key structure for table consumible
+-- ----------------------------
+ALTER TABLE "public"."consumible" ADD CONSTRAINT "platillo_pkey" PRIMARY KEY ("id_consumible");
+
+-- ----------------------------
+-- Primary Key structure for table etiqueta
+-- ----------------------------
+ALTER TABLE "public"."etiqueta" ADD CONSTRAINT "etiqueta_pkey" PRIMARY KEY ("id_etiqueta");
+
+-- ----------------------------
+-- Primary Key structure for table etiqueta_restaurante
+-- ----------------------------
+ALTER TABLE "public"."etiqueta_restaurante" ADD CONSTRAINT "etiqueta_restaurante_pkey" PRIMARY KEY ("id_restaurante_etiquetado", "id_etiqueta_restaurante");
+
+-- ----------------------------
+-- Primary Key structure for table menu
+-- ----------------------------
+ALTER TABLE "public"."menu" ADD CONSTRAINT "Menu_pkey" PRIMARY KEY ("id_menu");
+
+-- ----------------------------
+-- Primary Key structure for table registro_avistamiento
+-- ----------------------------
+ALTER TABLE "public"."registro_avistamiento" ADD CONSTRAINT "registro_avistamiento_pkey" PRIMARY KEY ("id_avistamiento");
+
+-- ----------------------------
+-- Primary Key structure for table resena_restaurante
+-- ----------------------------
+ALTER TABLE "public"."resena_restaurante" ADD CONSTRAINT "resena_restaurante_pkey" PRIMARY KEY ("id_resena");
+
+-- ----------------------------
+-- Primary Key structure for table restaurante
+-- ----------------------------
+ALTER TABLE "public"."restaurante" ADD CONSTRAINT "restaurante_pkey" PRIMARY KEY ("id_restaurante");
+
+-- ----------------------------
+-- Primary Key structure for table usuario
+-- ----------------------------
+ALTER TABLE "public"."usuario" ADD CONSTRAINT "usuario_pkey" PRIMARY KEY ("id_usuario");
+
+-- ----------------------------
+-- Foreign Keys structure for table consumible
+-- ----------------------------
+ALTER TABLE "public"."consumible" ADD CONSTRAINT "fk_menu" FOREIGN KEY ("id_menu_fk") REFERENCES "public"."menu" ("id_menu") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table etiqueta_restaurante
+-- ----------------------------
+ALTER TABLE "public"."etiqueta_restaurante" ADD CONSTRAINT "etiqueta_fk" FOREIGN KEY ("id_etiqueta_restaurante") REFERENCES "public"."etiqueta" ("id_etiqueta") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."etiqueta_restaurante" ADD CONSTRAINT "restaurante_fk" FOREIGN KEY ("id_restaurante_etiquetado") REFERENCES "public"."restaurante" ("id_restaurante") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table menu
+-- ----------------------------
+ALTER TABLE "public"."menu" ADD CONSTRAINT "fk_restaurante" FOREIGN KEY ("id_restaurante_fk") REFERENCES "public"."restaurante" ("id_restaurante") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table registro_avistamiento
+-- ----------------------------
+ALTER TABLE "public"."registro_avistamiento" ADD CONSTRAINT "fk_avistador" FOREIGN KEY ("id_usuario_fk") REFERENCES "public"."usuario" ("id_usuario") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- ----------------------------
+-- Foreign Keys structure for table resena_restaurante
+-- ----------------------------
+ALTER TABLE "public"."resena_restaurante" ADD CONSTRAINT "resena_restaurante_id_restaurante_fkey" FOREIGN KEY ("id_restaurante") REFERENCES "public"."restaurante" ("id_restaurante") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."resena_restaurante" ADD CONSTRAINT "resena_restaurante_id_usuario_fkey" FOREIGN KEY ("id_usuario") REFERENCES "public"."usuario" ("id_usuario") ON DELETE NO ACTION ON UPDATE NO ACTION;
