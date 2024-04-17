95d737c9bc7bd850da7ecb62d5c1f344bf7050f0
PHansLM <105395378+PHansLM@users.noreply.github.com>
2024-04-16 22:50:23 -0400

    constructor de imagenes general

--- noraa/src/app/principal/ImgConstructor.tsx
+++ noraa/src/app/principal/ImgConstructor.tsx
@@ -1,18 +1,20 @@
-const ImgConstructor = ({ imgBytea }: { imgBytea: Uint8Array }) => {
-    let counter = 0;
+import React from 'react';
 
-    const construirImagenUrl = (imgBytea: Uint8Array): string => {
-        if (imgBytea) {
-            const blob = new Blob([imgBytea], { type: 'image/jpg' });
-            const imageUrl = URL.createObjectURL(blob);
-            console.log('imagen cargada:' + imageUrl);
-            return imageUrl;
-        } else {
-            return ''; 
-        }
+interface ImgConstructorProps {
+    imgBytea: string;
+}
+
+const ImgConstructor: React.FC<ImgConstructorProps> = ({ imgBytea }) => {
+    const construirUrlImagen = (imgBytea: string) => {
+        // Convertir cadena base64 a un ArrayBuffer
+        const arrayBuffer = Buffer.from(imgBytea, 'base64').buffer;
+        // Crear un Blob a partir del ArrayBuffer
+        const blob = new Blob([arrayBuffer], { type: 'image/jpeg' });
+        // Crear una URL para el Blob
+        return URL.createObjectURL(blob);
     };
 
-    return construirImagenUrl(imgBytea);
+    return <img src={construirUrlImagen(imgBytea)} style={{ maxWidth: '100%', maxHeight: '100%' }} />;
 };
 
 export default ImgConstructor;
