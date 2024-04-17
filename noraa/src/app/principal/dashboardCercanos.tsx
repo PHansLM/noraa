5b2fba17d205794ddd4a8a0e1b5b153af6472606
Byskull2000 <grajedadiego1@gmail.com>
2024-04-16 22:29:39 -0400

    Aplicacion de estilos para el dashboard de restaurantes cercanos

--- noraa/src/app/principal/dashboardCercanos.tsx
+++ noraa/src/app/principal/dashboardCercanos.tsx
@@ -13,7 +13,7 @@ const DashboardCercanos = ({ centro }: MapPanelProps) => {
 
         async function cargarCercanos(lat: number, lng: number) {
             try {
-                const response = await fetch(`http://localhost:5000/restaurantes-cercanos-limitados?latitud=${lat}&longitud=${lng}&limite=${10}`);
+                const response = await fetch(`http://localhost:5000/restaurantes-cercanos-limitados?latitud=${lat}&longitud=${lng}&limite=${8}`);
                 const data = await response.json();
                 setRestaurantesC(data);
             } catch (error) {
@@ -24,20 +24,27 @@ const DashboardCercanos = ({ centro }: MapPanelProps) => {
     }, [centro]);
 
     return (
-        <div className="mt-1 ml-1 mr-1 bg-orange-200" style={{ height: '30vh', width: '100%' }}>
-            {restaurantesC.map((restaurante) => (
-                <div key={restaurante.id_restaurante} className="" style={{ height: '35%', width: '35%' }}>
-                    {restaurante.imagen && (
-                        //<img src={ImgConstructor({ imgBytea: restaurante.imagen })} style={{ maxWidth: '100%', maxHeight: '100%' }} alt="imagen restaurante" />
-                        <div></div>
-                    )}
-                    <h3 className="font-bold">{restaurante.nombre_restaurante}</h3>
-                    <p>{restaurante.horario_atencion}</p>
-                    <p>{restaurante.valoracion}</p>
-                </div>
-            ))}
+        <div className="mt-1 ml-1 mr-1 bg-white" style={{ height: 'auto', width: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+    {restaurantesC.map((restaurante) => (
+        <div key={restaurante.id_restaurante} className="card" style={{ height: 'auto', width: '45%', margin: '2%', padding: '10px', border: '1px solid black', borderRadius: '8px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+            {restaurante.imagen && (
+                //<img src={ImgConstructor({ imgBytea: restaurante.imagen })} style={{ maxWidth: '100%', maxHeight: '100%' }} alt="imagen restaurante" />
+                <div></div>
+            )}
+            <div style={{ marginTop: 'auto' }}>
+                <h3 className="font-bold">{restaurante.nombre_restaurante}</h3>
+                <p>{restaurante.horario_atencion}</p>
+                <p>{restaurante.valoracion}</p>
+            </div>
         </div>
+    ))}
+</div>
+
     );
 };
 
 export default DashboardCercanos;
+
+
+
+
