3b3d54f3cff3c094332185bbc085ac7cbbdcbf69
EleonorAnturiano <cami778anturiano@gmail.com>
2024-05-20 11:10:01 -0400

    Validacion en los registros y login

--- noraa/src/app/login/page.tsx
+++ noraa/src/app/login/page.tsx
@@ -1,12 +1,7 @@
 "use client";
-//import Spline from '@splinetool/react-spline';
+
 import React, { useEffect, useState } from 'react';
-import Modal from 'react-modal';
-import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
-import useUserLocation from '../utiles/geolocalizadores/useUserLocation';
 import 'leaflet/dist/leaflet.css';
-import useGeocoder from '../utiles/geolocalizadores/Geocoder';
-import TagPopup from '../elementos/TagPopUp';
 import { loggearUsuario } from '../utiles/consultores/usuarios';
 
 const Page: React.FC = () => {
@@ -40,14 +35,14 @@ const Page: React.FC = () => {
             setIntentoSumbit(true);
             return;
         }
-        loggearUsuario(correo ,password)
+        loggearUsuario(correo, password)
         .then(data => {
-          if(data.length==0){
-            console.error('Correo o contrasena incorrecta');
-          }else{
-            console.log('loggeado');
-            setInicioExitoso(true);
-          }
+            if (data.length == 0) {
+                console.error('Correo o contraseña incorrecta');
+            } else {
+                console.log('Logueado');
+                setInicioExitoso(true);
+            }
         })
     };
 
@@ -57,11 +52,11 @@ const Page: React.FC = () => {
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                            Inicio de sesion
+                            Inicio de sesión
                         </h1>
                         <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
                             <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Correo electronico </label>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Correo electrónico</label>
                                 <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-400 focus:border-orange-400 block w-full p-2.5" placeholder="usuario@email.com" 
                                         value={correo}
                                         onChange={(e) => setCorreo(e.target.value)}
@@ -78,17 +73,17 @@ const Page: React.FC = () => {
                             <div className="flex items-center justify-between">
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 " required />
+                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" />
                                     </div>
                                     <div className="ml-3 text-sm">
-                                        <label htmlFor="remember" className="text-gray-500">Guardar informacion</label>
+                                        <label htmlFor="remember" className="text-gray-500">Guardar información</label>
                                     </div>
                                 </div>
-                                <a href="#" className="text-sm font-medium text-orange-400 hover:underline">Olvidaste tu contraseña?</a>
+                                <a href="#" className="text-sm font-medium text-orange-400 hover:underline">¿Olvidaste tu contraseña?</a>
                             </div>
-                            <button type="submit" className="w-full text-white bg-orange-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-full text-sm px-5 py-2.5 text-center ">Iniciar sesion</button>
+                            <button type="submit" className="w-full text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-full text-sm px-5 py-2.5 text-center">Iniciar sesión</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                               No tienes cuenta en Noraa? <a href="/register" className="font-medium text-orange-400 hover:underline">Registrate</a>
+                                ¿No tienes cuenta en Noraa? <a href="/register" className="font-medium text-orange-400 hover:underline">Regístrate</a>
                             </p>
                         </form>
                     </div>
@@ -96,8 +91,8 @@ const Page: React.FC = () => {
                 {inicioExitoso && (
                     <div className="fixed bottom-0 left-0 w-full bg-green-500 flex justify-center items-center p-4">
                         <div>
-                            <h1 className="text-2xl font-bold text-white">¡Sesion iniciada correctamente!</h1>
-                            <p className="text-white">{`Cerrando la página automáticamente en ${contador} segundos`}</p>
+                            <h1 className="text-2xl font-bold text-white">¡Sesión iniciada correctamente!</h1>
+                            <p className="text-white">{Cerrando la página automáticamente en ${contador} segundos}</p>
                         </div>
                     </div>
                 )}
