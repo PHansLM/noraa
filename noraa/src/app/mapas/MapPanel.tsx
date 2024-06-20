b10d5c85f9a087015f2096f95f7947b6e7b7a97a
PHansLM <105395378+PHansLM@users.noreply.github.com>
2024-06-20 00:57:27 -0400

    Correcion de MapPanel.tsx

--- noraa/src/app/mapas/MapPanel.tsx
+++ noraa/src/app/mapas/MapPanel.tsx
@@ -7,14 +7,6 @@ import { cargarAvistamientos } from '../utiles/consultores/avistamientos';
 import ImgConstructor from '../utiles/multimedia/ImgConstructor';
 import { useRouter } from 'next/navigation';  
 
-const StarIcon = () => (
-  <span className="ml-1 sm:ml-2">
-    <svg className="h-4 w-4 sm:h-6 sm:w-6" viewBox="0 0 47.94 47.94" fill="#ede607">
-      <path d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956 C22.602,0.567,25.338,0.567,26.285,2.486z"/>
-    </svg>
-  </span>
-);
-
 interface MapPanelProps {
   centro: [number, number];
 }
@@ -26,29 +18,33 @@ const MapPanel: React.FC<MapPanelProps> = ({ centro }: MapPanelProps) => {
   const [showDetail, setShowDetail] = useState(false);
   const router = useRouter();
 
-
   const handleCardClick = (id: string) => {
     localStorage.setItem('selectedRestaurantId', id);
-    router.push('/paginaRestaurante'); // Redirige usando next/router
+    if (typeof window !== 'undefined') {
+      router.push('/paginaRestaurante'); // Redirige usando next/router
+    }
   };
 
   useEffect(() => {
-    // Cargar todos los restaurantes
-    cargarRestaurantes()
-      .then(data => {
-        setRestaurantes(data);
-      })
-      .catch(error => {
-        console.error('Error al cargar todos los restaurantes:', error);
-      });
+    // Asegurarse de que solo se ejecute en el cliente
+    if (typeof window !== 'undefined') {
+      // Cargar todos los restaurantes
+      cargarRestaurantes()
+        .then(data => {
+          setRestaurantes(data);
+        })
+        .catch(error => {
+          console.error('Error al cargar todos los restaurantes:', error);
+        });
 
-    cargarAvistamientos()
-      .then(data => {
-        setAvistamientos(data);
-      })
-      .catch(error => {
-        console.error('Error al cargar avistamientos en el map Panel', error);
-      });
+      cargarAvistamientos()
+        .then(data => {
+          setAvistamientos(data);
+        })
+        .catch(error => {
+          console.error('Error al cargar avistamientos en el map Panel', error);
+        });
+    }
   }, [centro]);
 
   const handleMapClick = () => {
@@ -59,6 +55,11 @@ const MapPanel: React.FC<MapPanelProps> = ({ centro }: MapPanelProps) => {
     setRestauranteSeleccionado(restaurante);
     setShowDetail(true);
   };
+
+  // Asegurarse de que solo se renderiza en el cliente
+  if (typeof window === 'undefined') {
+    return null;
+  }
 
   return (
     <div onClick={handleMapClick}>
@@ -102,25 +103,6 @@ const MapPanel: React.FC<MapPanelProps> = ({ centro }: MapPanelProps) => {
               </Popup>
             </Marker>
           ))}
-
-          {avistamientos.map((avistamiento) => (
-            <Marker
-              key={avistamiento.id_avistamiento}
-              position={[avistamiento.coordenada_latitud, avistamiento.coordenada_longitud]}
-              icon={L.icon({
-                iconUrl: avistamiento.icono + ".png",
-                iconSize: [32, 32],
-                iconAnchor: [16, 16],
-                popupAnchor: [0, -16],
-              })}
-            >
-              <Popup>
-                <div>
-                  <h3>{avistamiento.nombre_restaurante}</h3>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
         </MapContainer>
 
         {showDetail && restauranteSeleccionado && (
@@ -152,7 +134,6 @@ const MapPanel: React.FC<MapPanelProps> = ({ centro }: MapPanelProps) => {
                 <div className="flex flex-col p-4 leading-normal text-left sm:text-left mr-12" style={{ width: '48%' }}>
                   <h5 className="mb-2 lg:text-xl font-bold tracking-tight text-gray-900 dark:text-white text-sm">{restauranteSeleccionado.nombre_restaurante}</h5>
                   <p className="mt-1 font-normal text-xs lg:text-sm text-gray-700 text-left">Hora de entrada: {restauranteSeleccionado.horario_atencion}</p>
-                  <p className="mt-1 mb-1 font-normal text-xs sm:text-sm text-gray-700 flex items-center text-left">Calificación: {restauranteSeleccionado.valoracion} <StarIcon /></p>
                 </div>
             </a>
           </div>
